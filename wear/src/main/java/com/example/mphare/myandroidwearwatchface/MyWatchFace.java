78e070040a3ef901d6883aa51368554e4843684a
Michael Hare <mphare@whitehare.com>
2015-05-09 18:03:04 -0500

    Initial Commit

---
+++ wear/src/main/java/com/example/mphare/myandroidwearwatchface/MyWatchFace.java
@@ -0,0 +1,28 @@
+package com.example.mphare.myandroidwearwatchface;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+public class MyWatchFace extends Activity
+{
+
+  private TextView mTextView;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_my_watch_face);
+    final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+    stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener()
+    {
+      @Override
+      public void onLayoutInflated(WatchViewStub stub)
+      {
+        mTextView = (TextView) stub.findViewById(R.id.text);
+      }
+    });
+  }
+}
